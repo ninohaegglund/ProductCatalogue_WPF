68cc7ab746c25d4118b880db2e4d5bef747bea20
Nino Hägglund <ninomh99@gmail.com>
2024-10-02 11:30:41 +0200

    Add project files.

---
+++ ProductCatalogue.Shared/Activity.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductCatalogue.Shared;
+
+public class Activity
+{
+}

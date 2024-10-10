90df4459536aa6645275b214e15af99e1d98fd1d
Nino Hägglund <ninomh99@gmail.com>
2024-10-10 06:23:00 +0200

    Added a test for FileService

---
+++ Shared.Tests/FileService_Tests.cs
@@ -0,0 +1,24 @@
+﻿using Shared.Services;
+
+namespace Shared.Tests;
+
+public class FileService_Tests
+{
+    private readonly string _fakeFilePath = Path.Combine(Path.GetTempPath(),"fakeProducts.json");
+    private readonly IFileService _fileService;
+
+    public FileService_Tests()
+    {
+        _fileService = new FileService(_fakeFilePath);
+    }
+    [Fact]
+    public void SaveToFile_ShouldSaveTextToJsonFile()
+    {
+
+    }
+    [Fact]
+    public void LoadFromFile_ShouldGetTextFromJsonFile()
+    {
+
+    }
+}
